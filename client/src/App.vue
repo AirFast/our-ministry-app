685bdb221f0e3e7339236664d3fdcebb936101bb
AirFast <airfast.88@gmail.com>
2022-06-18 20:48:41 +0300

    feat: add lang to header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
+import { watch } from 'vue'
 import { RouterView, RouterLink } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { pages } from '~/pages'
 
-const { t } = useI18n()
-const routs = pages.map(({path, name}) => ({ path, name, isBtn: ['login', 'register'].includes(name as string) }))
+const { locale, t } = useI18n()
+const routs = pages.map(({path, name}) => ({ 
+    path, name, isBtn: ['login', 'register'].includes(name as string) 
+  })
+)
+
+watch(locale, (locale) => {
+  document.documentElement.setAttribute('lang', locale)
+})
 </script>
 
 <template>
-  <header class="py-6 divide-y divide-slate-100">
-    <nav>
+  <header class="py-6">
+    <nav class="flex items-center justify-between">
+      <form>
+        <select v-model="locale">
+          <option value="en">en</option>
+          <option value="uk">uk</option>
+        </select>
+      </form>
       <ul class="-mx-4 text-right" :class="{'-mr-2': routs[routs.length - 1].isBtn}">
         <li v-for="{path, name, isBtn} in routs" class="inline-block">
           <RouterLink :to="path" class="text-sm font-medium block px-4 transition-colors duration-200 ease-out" :class="[{'link-button': isBtn}, {'hover:text-indigo-500': !isBtn}]">{{ t(name as string) }}</RouterLink>
--- client/src/pages/home/HomePage.vue
+++ client/src/pages/home/HomePage.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-import { watch } from 'vue';
 import { useI18n } from 'vue-i18n'
 
-const { locale, t } = useI18n()
-
-watch(locale, (locale) => {
-  document.documentElement.setAttribute('lang', locale)
-})
+const { t } = useI18n()
 </script>
 
 <template>
-  <form>
-    <select v-model="locale">
-      <option value="en">en</option>
-      <option value="uk">uk</option>
-    </select>
-  </form>
   <h1 class="text-5xl">{{ t('hello') }}</h1>
   <p>Головна сторінка</p>
 </template>
