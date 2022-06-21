871be58e00c3673f0d821565a5a0f2003a1476ab
AirFast <airfast.88@gmail.com>
2022-06-21 12:36:46 +0300

    feat: pass input

--- client/src/pages/login/LoginPage.vue
+++ client/src/pages/login/LoginPage.vue
@@ -42,11 +42,11 @@ const isPassVisible = ref(false)
     <form @submit.prevent="formLoginSubmit" name="login" class="w-full max-w-md px-12 pt-8 pb-10 rounded-md ring-1 ring-indigo-500/5 shadow-md bg-slate-50 dark:bg-slate-800">
       <h1 class="text-xl font-medium tracking-tight text-center mb-7">{{ t('login') }}</h1>
       <div class="mb-6 last-of-type:mb-0">
-        <input v-model="formLogin.email" class="w-full border rounded px-4 py-3 outline-none duration-200 focus:border-indigo-300 dark:text-slate-500" type="email" placeholder="Email" />
+        <input v-model="formLogin.email" class="w-full border rounded px-4 py-3 outline-none duration-200 focus:border-indigo-300 dark:text-slate-500" type="email" placeholder="email" />
       </div>
       <div class="mb-6 relative last-of-type:mb-0">
-        <input v-model="formLogin.password" ref="passRef" class="w-full border rounded px-4 py-3 outline-none duration-200 focus:border-indigo-300 dark:text-slate-500" :type="isPassVisible ? 'password' : 'text'" placeholder="Password" />
-        <span @click="isPassVisible = !isPassVisible" class="absolute top-0 right-0 h-full cursor-pointer flex items-center px-4 duration-200 text-slate-300 hover:text-slate-400">
+        <input v-model="formLogin.password" :type="isPassVisible ? 'password' : 'text'" placeholder="password" class="w-full border rounded px-4 py-3 outline-none duration-200 focus:border-indigo-300 dark:text-slate-500" :class="{'pr-14': true}" />
+        <span @click="isPassVisible = !isPassVisible" class="absolute top-0 right-0 h-full cursor-pointer flex items-center px-4 duration-200 text-slate-400 hover:text-slate-300">
           <EyeIcon v-if="isPassVisible" class="w-6 h-6" />
           <EyeOffIcon v-else class="w-6 h-6" />
         </span>
